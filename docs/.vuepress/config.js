6fe792e18024b3054bcd5474de5e8bfe724dc8d5
zhangting5 <zhangting5@xiaomi.com>
2022-01-27 23:48:10 +0800

    update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // 站点配置
+    lang: 'zh-CN',
+    title: '婷婷小站 ！',
+    description: '',
+    base: '/',
+
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+        displayAllHeaders: true, // 默认值：false
+        activeHeaderLinks: false, // 默认值：true
+    },
+}
