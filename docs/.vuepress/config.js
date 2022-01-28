72a4389af28ecb6b25741617e545751cd85d503b
zhangting5 <zhangting5@xiaomi.com>
2022-01-28 14:14:40 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,10 @@ module.exports = {
     // 站点配置
     lang: 'zh-CN',
     title: '婷婷小站 ！',
-    description: '',
+    description: 'vuepress',
     base: '/',
 
     // 主题和它的配置
-    theme: '@vuepress/theme-default',
     themeConfig: {
         lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
         displayAllHeaders: true, // 默认值：false
